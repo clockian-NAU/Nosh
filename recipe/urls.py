0cbfc84966bcaf98f42ded7201536af17e713c4f
Jasque Saydyk <jrsaydyk@gmail.com>
2018-03-30 20:18:52 -0700

    Inital Nosh website

---
+++ recipe/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [ 
+	url(r'^$', views.recipe_list, name='recipe_list'),
+	url(r'^recipe/(?P<pk>\d+)/$', views.recipe_detail, name='recipe_detail'),
+	url(r'^recipe/new/$', views.recipe_new, name='recipe_new'),
+	url(r'^recipe/(?P<pk>\d+)/edit/$', views.recipe_edit, name='recipe_edit'),
+]
---
+++ recipe/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from .models import Recipe
+from .forms import RecipeForm
+
+# Create your views here.
+def recipe_list(request):
+	recipes = Recipe.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'recipe/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, pk):
+	recipe = get_object_or_404(Recipe, pk=pk)
+	return render(request, 'recipe/recipe_detail.html', {'recipe': recipe})
+
+def recipe_new(request):
+	if request.method == "POST":
+		form = RecipeForm(request.POST)
+
+		if form.is_valid():
+			recipe = form.save(commit=False)
+			recipe.author = request.user
+			recipe.published_date = timezone.now()
+			recipe.save()
+			return redirect('recipe_detail', pk=recipe.pk)
+	else:
+		form = RecipeForm()
+
+	return render(request, 'recipe/recipe_edit.html', {'form': form})
+
+def recipe_edit(request, pk):
+	recipe = get_object_or_404(Recipe, pk=pk)
+	if request.method == "POST":
+		form = RecipeForm(request.POST, instance=recipe)
+		if form.is_valid():
+			recipe = form.save(commit=False)
+			recipe.author = request.user
+			recipe.published_date = timezone.now()
+			recipe.save()
+			return redirect('recipe_detail', pk=recipe.pk)
+	else:
+		form = RecipeForm(instance=recipe)
+	return render(request, 'recipe/recipe_edit.html', {'form': form})
