71d9c1fab6b33f82c30880c491985b78645bad0f
Jasque Saydyk <jrsaydyk@gmail.com>
2018-04-27 22:25:15 -0700

    Basic search added

--- recipe/urls.py
+++ recipe/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	url(r'^recipe/(?P<pk>\d+)/$', views.recipe_detail, name='recipe_detail'),
 	url(r'^recipe/new/$', views.recipe_new, name='recipe_new'),
 	url(r'^recipe/(?P<pk>\d+)/edit/$', views.recipe_edit, name='recipe_edit'),
+	url(r'^recipe/search/$', views.search, name='search'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from .models import Recipe
 from .forms import RecipeForm
+from django.db.models import Q
+import operator
+import re
 
 # Create your views here.
 def recipe_list(request):
@@ -42,3 +45,37 @@ def recipe_edit(request, pk):
 	else:
 		form = RecipeForm(instance=recipe)
 	return render(request, 'recipe/recipe_edit.html', {'form': form})
+
+def search(request):
+	query_string = ''
+	found_entries = None
+
+	if ('q' in request.GET) and request.GET['q'].strip():
+		query_string = request.GET['q']
+		entry_query = get_query(query_string, ['title', 'ingredients',])
+		found_entries = Recipe.objects.filter(entry_query).order_by('-published_date')
+	return render(request, 'recipe/search_results.html',
+		{ 'query_string': query_string, 'found_entries': found_entries})
+
+# Support Function
+def normalize_query(query_string,
+					findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+					normspace=re.compile(r'\s{2,}').sub):
+	return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
+def get_query(query_string, search_fields):
+	query = None
+	terms = normalize_query(query_string)
+	for term in terms:
+		or_query = None
+		for field_name in search_fields:
+			q = Q(**{"%s__icontains" % field_name: term})
+			if or_query is None:
+				or_query = q
+			else:
+				or_query = or_query | q
+		if query is None:
+			query = or_query
+		else:
+			query = query & or_query
+	return query
