965843e0735c71395984383801520c130a05d285
Jasque Saydyk <jrsaydyk@gmail.com>
2018-05-01 11:50:06 -0700

    Implemented displaying of uploaded images

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [ 
@@ -7,4 +9,4 @@ urlpatterns = [
 	url(r'^recipe/new/$', views.recipe_new, name='recipe_new'),
 	url(r'^recipe/(?P<pk>\d+)/edit/$', views.recipe_edit, name='recipe_edit'),
 	url(r'^recipe/search/$', views.search, name='search'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
